69b6070f2dedcd15bbc8a675af5e4d9dc3aa81c7
XuZhenkui <xuzhenk@bsoft.com.cn>
2020-08-31 09:24:54 +0800

    随便写写

--- src/main/java/Thread/threadpool/ThreadPoolExecutorFactory.java
+++ src/main/java/Thread/threadpool/ThreadPoolExecutorFactory.java
@@ -13,7 +13,7 @@ import java.util.concurrent.*;
  */
 public class ThreadPoolExecutorFactory {
 //    核心线程数量: 取 CPU 核心数 / 2 + 1 个
-    private final static int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() % 2 + 1;
+    private final static int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() / 2 + 1;
 
 //    最大线程池数量:
 //    1. CPU 密集型任务配置尽可能少的线程数量, 一般公式: CPU 核心数 + 1个线程的线程池
@@ -31,7 +31,7 @@ public class ThreadPoolExecutorFactory {
 
     private static ExecutorService threadPoolExecutor = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE,
             KEEP_ALIVE_TIME, TIME_UNIT,
-            workQueue, new ThreadPoolExecutor.AbortPolicy());
+            workQueue,threadFactory, new ThreadPoolExecutor.AbortPolicy());
 
     public static ExecutorService getThreadPoolExecutor() {
         return threadPoolExecutor;
